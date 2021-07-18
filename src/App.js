1768d5d947f4c6412f5d172c51bcece6ef174b74
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2021-07-18 22:49:52 +0300

    Session Storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { useState } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import Login from './components/Login'
 import Preferences from './components/Preferences'
 
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken))
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token')
+  const userToken = JSON.parse(tokenString)
+  return userToken?.token
+}
+
 function App() {
-  const [token, setToken] = useState()
+  const token = getToken()
 
   if (!token) {
     return <Login setToken={setToken} />
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,52 @@
-const Login = () => {
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+
+const Login = ({ setToken }) => {
+    const [username, setUserName] = useState()
+    const [password, setPassword] = useState()
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        const token = await loginUser({
+            username,
+            password
+        })
+        setToken(token)
+    }
+
     return (
         <div className='login-wrapper'> 
             <h1>Place Log In</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type='text' />
+                    <input type='text' onChange={e => setUserName(e.target.value)} />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type='password' />
+                    <input type='password' onChange={e => setPassword(e.target.value)} />
                 </label>
                 <div>
                     <button type='submit'>Submit</button>
-                </div>
+                </div>  
             </form>
         </div>
     )
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
 }
 
 export default Login
