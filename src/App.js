5c4c5eea776282870eb80ea60a800dbf6cfa7d04
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2021-07-18 08:46:06 +0300

    Login page & React route

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import Preferences from './components/Preferences'
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
