b9dc2c0d01c6ca3e82ac4661c9083fbf3f95ebab
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2021-07-18 15:17:26 +0300

    Form Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useState } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
+import Login from './components/Login'
 import Preferences from './components/Preferences'
 
 function App() {
+  const [token, setToken] = useState()
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className='wrapper'>
       <h1>Application</h1>
